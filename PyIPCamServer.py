70c9879e728be59d3dea8134fb321f0bd4fe80e7
unknown <dave.just@gmail.com>
2022-05-19 09:48:55 -0400

    Fixed resoltion parsing

--- PyIPCamServer.py
+++ PyIPCamServer.py
@@ -14,24 +14,22 @@ from numpy import vdot
 parser = argparse.ArgumentParser()
 parser.add_argument("-port", dest="serv_port" , type=str, default='8000', help="Server TCP Port Number")
 parser.add_argument("-index", dest="cam_index" , type=int, default = 0, help="Camera index number")
-parser.add_argument("-width", dest="img_width" , type=str, default = '480', help="Camera Width in pixels")
-parser.add_argument("-height", dest="img_height" , type=str, default = '600', help="Camera Width in pixels")
+parser.add_argument("-width", dest="img_width" , type=int, default = 640, help="Camera Width in pixels")
+parser.add_argument("-height", dest="img_height" , type=int, default = 480, help="Camera Width in pixels")
 
 
 args = vars(parser.parse_args())
-
-
+resolution=(int(args['img_width']), int(args['img_height']))
 lock = threading.Lock()
-video = VideoStream(src=args['cam_index'], resolution=(args['img_height'], args['img_width'])).start()
-
+video = VideoStream(src=args['cam_index']).start()
+print(f"Selected resolution : {resolution}")
 def vid_stream(stream_type):
-	# grab global references to the output frame and lock variables
 	global lock
-	
-	# loop over frames from the output stream
-	
+	global resolution
+	global video		
 	while True:
-		frame = video.read()
+		#frame = video.read()
+		frame = imutils.resize(video.read(), width=resolution[0], height=resolution[1])
 		with lock:
 			stream_output = frame.copy()
 			if stream_output is None:
