ca825e5b01de6092f84081600e138689e6868dc6
unknown <dave.just@gmail.com>
2022-05-19 09:30:19 -0400

    Added arguments

--- PyIPCamServer.py
+++ PyIPCamServer.py
@@ -10,9 +10,19 @@ import time
 import cv2
 from numpy import vdot
 
+# define arguments 
+parser = argparse.ArgumentParser()
+parser.add_argument("-port", dest="serv_port" , type=str, default='8000', help="Server TCP Port Number")
+parser.add_argument("-index", dest="cam_index" , type=int, default = 0, help="Camera index number")
+parser.add_argument("-width", dest="img_width" , type=str, default = '480', help="Camera Width in pixels")
+parser.add_argument("-height", dest="img_height" , type=str, default = '600', help="Camera Width in pixels")
+
+
+args = vars(parser.parse_args())
+
 
 lock = threading.Lock()
-video = VideoStream(src=0).start()
+video = VideoStream(src=args['cam_index'], resolution=(args['img_height'], args['img_width'])).start()
 
 def vid_stream(stream_type):
 	# grab global references to the output frame and lock variables
